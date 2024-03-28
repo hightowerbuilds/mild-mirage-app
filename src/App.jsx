60bb8c285ae6711d46627a0c332d13caeee8cae2
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-03-27 20:01:31 -0700

    march27.4

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Booking from './pages/Booking'
+import Cabins from './pages/Cabins'
+import Checkin from './pages/Checkin'
+import Settings from './pages/Settings'
+import Users from './pages/Users'
+import Login from './pages/Login'
+import Account from './pages/Account'
+
+export default function App() {
+  return (
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/bookings' element={<Booking />} />
+        <Route path='/cabins' element={<Cabins />} />
+        <Route path= 'check-in' element={<Checkin />} />
+        <Route path='/settings' element={<Settings />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/account' element={<Account />} />
+    </Routes>
+  
+  )
+}
---
+++ src/pages/Checkin.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Checkin() {
+  return (
+    <div>check in </div>
+  )
+}
